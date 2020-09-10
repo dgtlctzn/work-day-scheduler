d2b38d2e54a831879eef3bfa51e67c172e142493
Joseph Perry <josephperry720@gmail.com>
2020-09-10 17:21:07 -0400

    changed for loop index to deal with 0-23 hour format

--- assets/index.js
+++ assets/index.js
@@ -2,9 +2,11 @@ var mainContainer = $(".container");
 var currentDay = $("#currentDay");
 
 var currentDateString = moment().format("MMMM, Do YYYY");
+var currentDatetime = moment();
+console.log(currentDatetime.hour());
 
-for (i = 9; i < 18; i++) {
-    var businessHour = moment().hour(i).format("hA")
+for (i = 0; i < 9; i++) {
+    var businessHour = moment().hour(i + 9).format("hA")
 
     var timeRow = $("<div>");
 
@@ -17,6 +19,8 @@ for (i = 9; i < 18; i++) {
     timeBlock.text(businessHour);
     textArea.addClass("col-sm-10 past");
     saveButton.addClass("col-sm-1 saveBtn");
+
+    // if (currentDatetime.hour() > i) 
 
     timeRow.append(timeBlock, textArea, saveButton);
     mainContainer.append(timeRow);
