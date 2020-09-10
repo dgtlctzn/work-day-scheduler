63e5f0a6186df3dcc256047fc6f11f58d3b6157f
Joseph Perry <josephperry720@gmail.com>
2020-09-10 17:07:09 -0400

    added moment.js to display current date and format hours

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,11 @@
-var mainContainer = $(".container")
+var mainContainer = $(".container");
+var currentDay = $("#currentDay");
 
-for (i = 0; i < 9; i++) {
+var currentDateString = moment().format("MMMM, Do YYYY");
+
+for (i = 9; i < 18; i++) {
+    var businessHour = moment().hour(i).format("hA")
+
     var timeRow = $("<div>");
 
     var timeBlock = $("<p>");
@@ -9,9 +14,12 @@ for (i = 0; i < 9; i++) {
 
     timeRow.addClass("row");
     timeBlock.addClass("col-sm-1 time-block");
+    timeBlock.text(businessHour);
     textArea.addClass("col-sm-10 past");
     saveButton.addClass("col-sm-1 saveBtn");
 
     timeRow.append(timeBlock, textArea, saveButton);
     mainContainer.append(timeRow);
 }
+
+currentDay.text(currentDateString);
